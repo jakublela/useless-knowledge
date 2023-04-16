6e4a1b977cc0a024851ac2cf7f0d1ee7757db223
Wilczur <55357707+jakublela@users.noreply.github.com>
2023-04-16 16:57:50 +0200

    coś tam działa w końcu

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ import './modules/sidebar/sidebar.css';
 const App = props => {
   return (
     <Container fluid>
-      <Row>
-        <Col xs={2} id="sidebar-wrapper">
+        <Col id="sidebar-wrapper" style={{color: 'red'}}>
           <Sidebar/>
         </Col>
-        <Col xs={10} id="page-content-wrapper">
-          This is a test
-        </Col>
-      </Row>
     </Container>
   );
 }
