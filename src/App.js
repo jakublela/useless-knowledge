8b6a0a0d004d8073c8aba6462f306e9d929f3e5e
SewGrab <seweryn.g1803@gmail.com>
2023-05-11 21:17:22 +0200

    Dropdown menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import {Container, Row, Col, Card, Form, Button} from 'react-bootstrap';
 import Sidebar from './modules/sidebar/sidebar.js';
 import './modules/sidebar/sidebar.css';
+import SideMenu from './modules/SideMenu/SideMenu.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const App = props => {
   return (
     <Container fluid>
         <Col id="sidebar-wrapper" style={{color: 'red'}}>
           <Sidebar/>
+          <SideMenu/>
         </Col>
     </Container>
   );
---
+++ src/modules/SideMenu/SideMenu.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Dropdown, DropdownButton, Nav, Navbar } from 'react-bootstrap';
+import './SideMenu.css';
+
+function SideMenu() {
+    return (
+        <Navbar variant='dark' bg='dark' expand='lg' className='navbar'>
+            <Container className='navbar-wrapper'>
+                <Navbar.Brand>Menu</Navbar.Brand>
+                    <Nav>
+                        <DropItem itemTitle='Random'/>
+                        <DropItem itemTitle='Categories'/>
+                        <DropItem itemTitle='Add your own'/>
+                        <DropItem itemTitle='Saved'/>
+                        <DropItem itemTitle='Random'/>
+                    </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+function DropItem({itemTitle}) {
+    return (
+        <DropdownButton drop='down-centered' className='dropdown-button' variant='secondary' title={itemTitle}>
+            <Dropdown.Item>Trivia</Dropdown.Item>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Facts</Dropdown.Item>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Riddles</Dropdown.Item>
+        </DropdownButton>
+    )
+    
+}
+
+export default SideMenu;
