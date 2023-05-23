112a0853a77bb05d6485f08ba3dfb83e6aaca7c3
SewGrab <seweryn.g1803@gmail.com>
2023-05-23 18:31:11 +0200

    Sidebar space-between

--- src/modules/sidebar/sidebar.js
+++ src/modules/sidebar/sidebar.js
@@ -5,46 +5,38 @@ import pfp from './default_pfp.jpg';
 
 const Sidebar = props => {
     return (
-        <Nav className="col-md-12 d-none d-md-block sidebar">
-            <div className="sidebar-sticky">
+        <Nav className="sidebar-sticky">
+            <nav className="sidebar">
                 <p>✨Useless✨<br/>✨Knowledge✨</p>
                 <hr/>
-                <Nav.Item>
-                    <DropItem itemTitle='Random' className='dropdown'/>
-                </Nav.Item>
-                <Nav.Item>
-                    <DropItem itemTitle='Categories' className='dropdown'/>
-                </Nav.Item>
-                <Nav.Item>
-                    <DropItem itemTitle='Create' className='dropdown'/>
-                </Nav.Item>
-                <Nav.Item>
-                    <DropItem itemTitle='Saved' className='dropdown'/>
-                </Nav.Item>
-                <Nav.Item>
-                    <DropItem itemTitle='Loose braincells' className='dropdown'/>
-                </Nav.Item>
-                <footer>
-                    <hr/>
-                    <div className="user-info">
-                        <img src={pfp} className="user" alt="pfp"/>
-                        <p className="user">Username</p>
-                    </div>
-                </footer>
-            </div>
+                <DropItem itemTitle='Random' className='dropdown'/>
+                <DropItem itemTitle='Categories' className='dropdown'/>
+                <DropItem itemTitle='Create' className='dropdown'/>
+                <DropItem itemTitle='Saved' className='dropdown'/>
+                <DropItem itemTitle='Loose braincells' className='dropdown'/>
+            </nav>
+            <footer>
+                <hr/>
+                <div className="user-info">
+                    <img src={pfp} className="user" alt="pfp"/>
+                    <p className="user">Username</p>
+                </div>
+            </footer>
         </Nav>
     );
 };
 
 function DropItem({itemTitle}) {
     return (
-        <DropdownButton drop='down-centered' className='dropdown-button' variant='secondary' title={itemTitle}>
-            <Dropdown.Item>Trivia</Dropdown.Item>
-            <Dropdown.Divider/>
-            <Dropdown.Item>Facts</Dropdown.Item>
-            <Dropdown.Divider/>
-            <Dropdown.Item>Riddles</Dropdown.Item>
-        </DropdownButton>
+        <Nav.Item>
+            <DropdownButton drop='down-centered' className='dropdown-button' variant='secondary' title={itemTitle}>
+                <Dropdown.Item>Trivia</Dropdown.Item>
+                <Dropdown.Divider/>
+                <Dropdown.Item>Facts</Dropdown.Item>
+                <Dropdown.Divider/>
+                <Dropdown.Item>Riddles</Dropdown.Item>
+            </DropdownButton>
+        </Nav.Item>
     )
     
 }
