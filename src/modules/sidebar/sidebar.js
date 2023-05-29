8225e6512fa42632638c17a31fb124fd727116b7
Wilczur <55357707+jakublela@users.noreply.github.com>
2023-05-29 21:01:19 +0200

    Overlay 0.1

--- src/modules/sidebar/sidebar.js
+++ src/modules/sidebar/sidebar.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import {Dropdown, DropdownButton, Nav} from 'react-bootstrap';
+import React, {useState, useRef} from 'react';
+import {Dropdown, DropdownButton, Nav, Overlay, Button} from 'react-bootstrap';
 import './sidebar.css';
 import pfp from './default_pfp.jpg';
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = props => {
+    const [show, setShow] = useState(false);
+    const target = useRef(null);
+
     return (
         <Nav className="sidebar-sticky">
             <nav className="sidebar">
@@ -31,7 +34,33 @@ const Sidebar = props => {
                     <hr/>
                     <div className="user-info">
                         <img src={pfp} className="user" alt="pfp"/>
-                        <p className="user">Username</p>
+                        <p className="user">
+                            Username<br/>
+                            <button variant="danger" ref={target} onClick={() => setShow(!show)} className="login">Pop-up</button>
+                            <Overlay target={target.current} show={show} placement="top">
+                                {({
+                                    placement: _placement,
+                                    arrowProps: _arrowProps,
+                                    show: _show,
+                                    popper: _popper,
+                                    hasDoneInitialMeasure: _hasDoneInitialMeasure,
+                                    ...props
+                                }) => (
+                                    <div {...props}
+                                        style={{
+                                            position: 'absolute',
+                                            backgroundColor: 'rgba(255, 100, 100, 0.85',
+                                            padding: '2px 10px',
+                                            color: 'white',
+                                            borderRadius: 3,
+                                            ...props.style,
+                                        }}>
+                                        Simple Tooltipen
+                                    </div>
+                                    )
+                                }
+                            </Overlay>
+                        </p>
                     </div>
                 </footer>
             </nav>
