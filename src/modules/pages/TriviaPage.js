31592e873a716e8066f2292fff84a05f8974d33a
SewGrab <seweryn.g1803@gmail.com>
2023-06-10 21:12:18 +0200

    Categories fix

--- src/modules/pages/TriviaPage.js
+++ src/modules/pages/TriviaPage.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { Trivia } from '../HandlingAPI/Trivia';
 import { FetchTrivia } from '../HandlingAPI/handleAPI';
 import '../Main/Main.css';
+import { useParams } from 'react-router';
 
-function RandomPage() { 
+export default function TriviaPage() { 
+    let { tag } = useParams();
     let url = "https://the-trivia-api.com/v2/questions?" + new URLSearchParams({limit: "4"});
-    
+    if (!tag) console.log(tag);
+
     return (
         <div className='main'>
             <h1>Quiz</h1>
@@ -13,5 +16,3 @@ function RandomPage() {
         </div>
     )
 }
-
-export default RandomPage;
